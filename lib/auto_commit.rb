98950fcc6074dcb16cecd2a771aff87a56bfc5a8
msnrhrk <masanolinks@gmail.com>
2020-01-18 12:55:27 +0900

    Add auto_commit

---
+++ lib/auto_commit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "auto_commit/version"
+require "auto_commit/command"
+require "auto_commit/commands/git"
+require "auto_commit/message"
+require "auto_commit/message"
+require "auto_commit/cli"
